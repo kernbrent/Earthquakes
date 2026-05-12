011fbfccd58cf5125a447d6d4fdab9192682d4f6
Brent Kern <kern.brent@gmail.com>
2026-05-12 13:50:54 -0500

    More style fix

--- js/mobile-nav.js
+++ js/mobile-nav.js
@@ -1,19 +1,10 @@
 function initializeMobileMenu() {
-
-    const mobileButton =
-        document.querySelector('.mobile-menu-btn');
-
-    const nav =
-        document.querySelector('nav');
+    const mobileButton = document.querySelector('.mobile-menu-btn');
+    const nav = document.querySelector('nav');
 
     if (mobileButton && nav) {
-
         mobileButton.addEventListener('click', () => {
-
             nav.classList.toggle('mobile-open');
-
         });
-
     }
-
 }
