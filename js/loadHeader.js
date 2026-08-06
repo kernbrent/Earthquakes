b74b1dc5c24ff96605e1bdd0ccf841b5c69e53cf
kernbrent <kern.brent@gmail.com>
2026-08-05 22:45:29 -0500

    footer update

--- js/loadHeader.js
+++ js/loadHeader.js
@@ -10,4 +10,17 @@ document.addEventListener("DOMContentLoaded", () => {
 
         });
 
+    const footer = document.querySelector('footer');
+    if (footer && !footer.querySelector('.developer-credit')) {
+        const credit = document.createElement('a');
+        credit.className = 'developer-credit';
+        credit.href = 'https://careersteps.net/';
+        credit.setAttribute('aria-label', 'Visit Career Steps Consulting LLC');
+        credit.innerHTML = `
+            <img src="/images/career-steps-logo.png" alt="" width="28" height="28">
+            <span>Website developed and maintained by <strong>Career Steps Consulting LLC.</strong></span>
+        `;
+        footer.appendChild(credit);
+    }
+
 });
